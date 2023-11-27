5363ba5874ffea880e618c3f0e8ff7858d73b5c0
Gabriel <fakcbos@gmail.com>
2023-11-27 15:18:27 -0300

    feat: testing beanie with cluster on Atlas/MongoDB

--- API/main.py
+++ API/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+test_user_db = [{"user_id": "1"}, {"user_id": "2"}, {"user_id": "3"}]
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return fake_items_db 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
@@ -17,3 +21,9 @@ def read_user_me():
 @app.get("/users/{user_id}")
 def read_user(user_id: str):
     return {"user_id": user_id}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
